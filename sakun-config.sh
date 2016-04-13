0d8094edf9b58d6d4c993509233b1231d566053d
Sakari Ellonen <sakari.ellonen@gmail.com>
2016-04-13 16:25:07 +0300

    not needed

--- sakun-config.sh
+++ sakun-config.sh
@@ -1,5 +1,3 @@
 cd ~/.vim_runtime
 
 git clone https://github.com/klen/python-mode.git sources_non_forked/python-mode
-git clone https://github.com/pangloss/vim-javascript sources_non_forked/
-git clone https://github.com/lambdatoast/elm.vim sources_non_forked/
